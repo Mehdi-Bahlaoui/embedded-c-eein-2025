15df1cd4a6fca1cfe3af3651f8988ba464e5ea81
mehdi <mehdibahlaoui3@gmail.com>
2025-11-02 21:40:48 +0100

    test3

--- BahlaouiMehdi/all.c
+++ BahlaouiMehdi/all.c
@@ -4,22 +4,28 @@
 const unsigned short CST = 2;
 const unsigned short DT = 1;
 unsigned short T = 0;
-short bitField;
 
 
 
-int32_t  all(unsigned char Cnt, unsigned char R, short b0, short b1, short b2)
+
+unsigned char Cnt, R;
+short b0, b1, b2, bitField;
+int input;
+unsigned int upper, lower, output;
+
+
+
+
+void  all(void)
 {
 
-    
-    
-    
-    
     
     
     bitField = b0 + (b1<<1) + (b2<<2);
 
      
+
+
     switch (R)
     {
     case 0:
@@ -32,7 +38,27 @@ int32_t  all(unsigned char Cnt, unsigned char R, short b0, short b1, short b2)
     default:
         Cnt = 0;
         break;
+    };
+
+
+
+
+    if ((output + input) <= upper && (output + input) >= lower)
+    {
+        output += input;
     }
+    else
+    {
+        if ((output + input) >= lower)
+        {
+            output = upper;
+        }
+        else
+        {
+            output = lower;
+        }
+    }
+   
 
 
 
--- BahlaouiMehdi/main.c
+++ BahlaouiMehdi/main.c
@@ -1,38 +1,52 @@
-#include "answer1.h"
-#include "answer2.h"
-#include "answer3.h"
-
 #include "all.c"
-
-// This file is just for running and testing the three different functions
-// gcc BahlaouiMehdi/main.c -o BahlaouiMehdi/main.exe &&  cd BahlaouiMehdi && main.exe
-
-
 #include <stdio.h>
-ClockState state1 = {0, 1};
+
 
 int main() {
 
-//     for(int i=0; i<10; i++) {
-//         int32_t result1 = top_left(0, &state1);
-//         printf("Result 1 (iteration %d): %d\n", i+1, result1);
-//     }
 
+    //TC1 - Initialize variables
+    Cnt=0, R=0, b0=0, b1=0, b2=0, bitField=0, input=0, upper=0, lower=1, output=1;
+    //Call of MUT
+    all();
+    //Checking Output
+    if (bitField == 0 && output == 0 && Cnt == 1) printf("Test1 passed\n"); else printf("Test1 Failed\n");
 
+    //TC2 - Initialize variables
+    Cnt=0, R=0, b0=0, b1=0, b2=0, bitField=0, input=0, upper=0, lower=0, output=1;
+    //Call of MUT
+    all();
+    //Checking Output
+    if (bitField == 0 && output == 0 && Cnt == 2) printf("Test2 passed\n"); else printf("Test2 Failed\n");
 
-
-
-
-//     int32_t b0 = 1, b1 = 2, b2 = 3;
-//     int32_t result2 = bitField(&b0, &b1, &b2);
-//     printf("Result 2: %d\n", result2);
-
-//     int32_t output;
-//     int32_t* prevOutput = &output;
-//     int32_t result3 = bottom_left(5, 10, 0, prevOutput);
-//     printf("Result 3: %d\n", result3);
-
+    //TC3 - Initialize variables
+    Cnt=0, R=0, b0=0, b1=0, b2=0, bitField=0, input=0, upper=0, lower=1, output=0;
+    //Call of MUT
+    all();
+    //Checking Output
+    if (bitField == 0 && output == 1 && Cnt == 3) printf("Test3 passed\n"); else printf("Test3 Failed\n");
     
+    //TC4 - Initialize variables
+    Cnt=3, R=0, b0=0, b1=0, b2=0, bitField=0, input=0, upper=0, lower=1, output=1;
+    //Call of MUT
+    all();
+    //Checking Output
+    if (bitField == 0 && output == 0 && Cnt == 3) printf("Test4 passed\n"); else printf("Test4 Failed\n");
+
+    //TC5 - Initialize variables
+    Cnt=3, R=0, b0=0, b1=0, b2=0, bitField=0, input=0, upper=0, lower=0, output=1;
+    //Call of MUT
+    all();
+    //Checking Output
+    if (bitField == 0 && output == 0 && Cnt == 3) printf("Test5 passed\n"); else printf("Test5 Failed\n");
+
+    //TC6 - Initialize variables
+    Cnt=3, R=0, b0=0, b1=0, b2=0, bitField=0, input=0, upper=0, lower=1, output=0;
+    //Call of MUT
+    all();
+    //Checking Output
+    if (bitField == 0 && output == 1 && Cnt == 3) printf("Test6 passed\n"); else printf("Test6 Failed\n");
+
 
 
     return 0;
