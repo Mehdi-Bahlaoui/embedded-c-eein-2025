fbed89fd71856c50b4f9db3a618fb33d33832536
Nabil AZELMAT <nab.azelmat@gmail.com>
2025-11-02 22:15:41 +0100

    Ajout des commentaires TCn

--- AzelmatNabil/TC_MBD/main.c
+++ AzelmatNabil/TC_MBD/main.c
@@ -48,27 +48,27 @@ void MBD(void)
 int main() {
 
     printf("=== DEBUT DES TESTS SIMPLES (Logique du Collegue) ===\n\n");
-
+//TC1
     Cnt=0; R=0; b0=0; b1=0; b2=0; bitField=0; input=0; upper=0; lower=1; output=1;
     MBD();
     if (bitField == 0 && output == 0 && Cnt == 1) printf("Test1 passed\n"); else printf("Test1 Failed\n");
-
+//TC2
     Cnt=0; R=0; b0=0; b1=0; b2=0; bitField=0; input=0; upper=0; lower=0; output=1;
     MBD();
     if (bitField == 0 && output == 0 && Cnt == 2) printf("Test2 passed\n"); else printf("Test2 Failed\n");
-
-    Cnt=0; R=0; b0=0; b1=0; b2=0; bitField=0; input=0; upper=0; lower=1; output=0;
+//TC3
+   Cnt=0; R=0; b0=0; b1=0; b2=0; bitField=0; input=0; upper=0; lower=1; output=0;
     MBD();
     if (bitField == 0 && output == 1 && Cnt == 3) printf("Test3 passed\n"); else printf("Test3 Failed\n");
-    
+ //TC4
     Cnt=3; R=0; b0=0; b1=0; b2=0; bitField=0; input=0; upper=0; lower=1; output=1;
     MBD();
     if (bitField == 0 && output == 0 && Cnt == 3) printf("Test4 passed\n"); else printf("Test4 Failed\n");
-
+//TC5
     Cnt=3; R=0; b0=0; b1=0; b2=0; bitField=0; input=0; upper=0; lower=0; output=1;
     MBD();
     if (bitField == 0 && output == 0 && Cnt == 3) printf("Test5 passed\n"); else printf("Test5 Failed\n");
-
+//TC6
     Cnt=3; R=0; b0=0; b1=0; b2=0; bitField=0; input=0; upper=0; lower=1; output=0;
     MBD();
     if (bitField == 0 && output == 1 && Cnt == 3) printf("Test6 passed\n"); else printf("Test6 Failed\n");
